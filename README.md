2c673de37edc21cdc0b4b67bf81dd3360170da02
zhangxiaoguang <zhangxiaoguang@digisky.com>
2021-02-23 15:17:16 +0800

    modify json2lua

--- README.md
+++ README.md
@@ -1,38 +1,12 @@
 # jsontolua
 a python script convert json to lua table
 
-#usage:
-git clone https://github.com/neoliang/jsontolua.git
+fork from https://github.com/neoliang/jsontolua.git
 
-cd jsontolua
 
-python ./setup.py install
+## 使用
 
-## Quick Expamples
+python jsontolua.py -i json_file -o lua_file
 
-#command line
-
-python ./convert_json.py json_file lua_file
-
-#code
-```python
-
-#convert string
-jsontolua.str_to_lua_table('{"a":"1","b":2,"c":[1,2,3]}')
-#return lua code 
-'''
-{
-	a = '1',
-	c = {
-		1,2,3
-	},
-	b = 2
-}
-
-'''
-#convert file
-jsontolua.file_to_lua_file('./1.json','./1.lua')
-
-```
 
 
--- jsontolua.py
+++ jsontolua.py
@@ -3,37 +3,37 @@ import json
 import types
 import json
 import os
+import getopt
+import sys
 
 def space_str(layer):
 	spaces = ""
-	for i in range(0,layer):
+	for i in range(0, layer):
 		spaces += '\t'
 	return spaces
 
 def dic_to_lua_str(data,layer=0):
-
-	d_type = type(data)
-	if  d_type is types.StringTypes or d_type is str or d_type is types.UnicodeType:
-		yield ("'" + data + "'")
-	elif d_type is types.BooleanType:
+	if isinstance(data, str) :
+		yield ("\"" + data + "\"")
+	elif isinstance(data, bool):
 		if data:
 			yield ('true')
 		else:
 			yield ('false')
-	elif d_type is types.IntType or d_type is types.LongType or d_type is types.FloatType:
+	elif isinstance(data, int) or isinstance(data, float):
 		yield (str(data))
-	elif d_type is types.ListType:
+	elif isinstance(data, list):
 		yield ("{\n")
-		yield (space_str(layer+1))
-		for i in range(0,len(data)):
-			for sub in  dic_to_lua_str(data[i],layer+1):
+		yield (space_str(layer + 1))
+		for i in range(0, len(data)):
+			for sub in  dic_to_lua_str(data[i], layer + 1):
 				yield sub
 			if i < len(data)-1:
 				yield (',')
 		yield ('\n')
 		yield (space_str(layer))
 		yield ('}')
-	elif d_type is types.DictType:
+	elif isinstance(data, dict):
 		yield ("\n")
 		yield (space_str(layer))
 		yield ("{\n")
@@ -41,33 +41,33 @@ def dic_to_lua_str(data,layer=0):
 		data_count = 0
 		for k,v in data.items():
 			data_count += 1
-			yield (space_str(layer+1))
-			if type(k) is types.IntType:
+			yield (space_str(layer + 1))
+			if isinstance(k, int):
 				yield ('[' + str(k) + ']')
 			else:
 				yield (k) 
 			yield (' = ')
 			try:
-				for sub in  dic_to_lua_str(v,layer +1):
+				for sub in  dic_to_lua_str(v,layer + 1):
 					yield sub
 				if data_count < data_len:
 					yield (',\n')
 
-			except Exception, e:
-				print 'error in ',k,v
+			except Exception as e:
+				print('error in ', k, v)
 				raise
 		yield ('\n')
 		yield (space_str(layer))
 		yield ('}')
 	else:
-		raise d_type , 'is error'
+		raise(d_type , 'is error')
 
 def str_to_lua_table(jsonStr):
 	data_dic = None
 	try:
 		data_dic = json.loads(jsonStr)
-	except Exception, e:
-		data_dic =[]
+	except Exception as e:
+		data_dic = []
 	else:
 		pass
 	finally:
@@ -76,7 +76,29 @@ def str_to_lua_table(jsonStr):
 	for it in dic_to_lua_str(data_dic):
 		bytes += it
 	return bytes
-def file_to_lua_file(jsonFile,luaFile):
+
+def file_to_lua_file(jsonFile, luaFile):
 	with open(luaFile,'w') as luafile:
 		with open(jsonFile) as jsonfile:
 			luafile.write(str_to_lua_table(jsonfile.read()))
+
+if __name__ == "__main__" :
+	opts, args = getopt.getopt(sys.argv[1:],'-i:-o:',['input','output'])
+	jsonFile = None
+	luaFile = None
+	for opt_name, opt_value in opts:
+		if opt_name in ('-i','--input'):
+			jsonFile = opt_value
+		if opt_name in ('-o','--output'):
+			luaFile = opt_value
+	if not jsonFile:
+		print("[error] no input file with param -i")
+		exit()
+	if not luaFile:
+		print("[error] no output file with param -o")
+		exit()
+
+	#file_to_lua_file("DT_CustomEquip.json", "DT_CustomEquip.lua")
+	file_to_lua_file(jsonFile, luaFile)
+
+	# python jsontolua.py -i DT_CustomEquip.json -o DT_CustomEquip.lua
